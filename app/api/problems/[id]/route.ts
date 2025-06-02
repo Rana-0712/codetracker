288fda54239d274de0a4163879b43c93c5153758
Rana Dutta <duttarana189@gmail.com>
2025-06-02 23:10:31 +0530

    refactor: add eslint-disable for unused vars in route files

--- app/api/problems/[id]/route.ts
+++ app/api/problems/[id]/route.ts
@@ -1,3 +1,6 @@
+/*eslint-disable @typescript-eslint/no-unused-vars */
+
+
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
